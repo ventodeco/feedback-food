1ea4274fb163bf4389b0fbcfc564d6d64eec0cf1
ventodeco <ventodecoboagoes@gmail.com>
2021-06-01 16:39:45 +0700

    add more response for feedback endpoint

--- app/Http/Controllers/FoodImageController.php
+++ app/Http/Controllers/FoodImageController.php
@@ -4,15 +4,50 @@ namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
 use App\Http\Requests\FeedbackFoodStoreRequest;
+use App\Repositories\FeedbackRepository;
 use Illuminate\Http\Request;
 
 class FoodImageController extends Controller
 {
+    public function __construct(FeedbackRepository $feedbackRepository)
+    {
+        $this->feedbackRepository = $feedbackRepository;
+    }
+
     public function store(FeedbackFoodStoreRequest $request)
     {
         // $path = $request->file('avatar')->store('avatars');
+        
+        $validated = $request->validated();
 
-        // return $path;
+        if (!$validated) {
+            return response()->json([
+                'status' => false,
+                'meta' => [
+                    'response_code' => 422,
+                    'code' => 422,
+                    'severity' => "OK",
+                    'messages' => "Request not passed validation"
+                ]
+            ]);
+        }
+
+        $validated['path'] = 'vento';
+
+        $feedback = $this->feedbackRepository->store($validated);
+
+        if (!$feedback) {
+            return response()->json([
+                'status' => false,
+                'meta' => [
+                    'response_code' => 422,
+                    'code' => 422,
+                    'severity' => "OK",
+                    'messages' => "Food doesn't stored successfully."
+                ]
+            ]);
+        }
+        
         return response()->json([
             'status' => true,
             'meta' => [
